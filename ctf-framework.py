62c95fbeec2aa17fc882109fa2fa8037ee3bb4f8
sloftus <sloftus@aegis-strategies.com>
2019-10-23 16:06:39 -0500

    initial commit

--- ctf-framework.py
+++ ctf-framework.py
@@ -1,32 +1,21 @@
-import consolemenu
 import os
 import subprocess
 import ctff_functions
-import time
 
 
 def subprocess_run(command):
     subprocess.run(command, shell=True)
 
 
-main_menu = consolemenu.ConsoleMenu("CTF Framework Setup", "Select your function")
-
 host_setup_folder = os.path.join(os.path.abspath("."), "host_setup")
 host_setup_folder_contents = os.listdir(host_setup_folder)
 
-setup_folder_list = []
+setup_modules = []
 for folder in host_setup_folder_contents:
     folder = os.path.join(host_setup_folder, folder)
     if os.path.isdir(folder):
-        setup_folder_list.append(folder)
-for setup_folder in setup_folder_list:
-    module_config = ctff_functions.config_parse(setup_folder)
-    setup_file = os.path.join(setup_folder, "setup.py")
-    setup_command = "python3 {}".format(setup_file)
-    print(setup_file)
-    print(setup_command)
+        setup_modules.append(ctff_functions.config_parse(folder))
 
-    setup_item = consolemenu.items.FunctionItem(module_config.prettyName, subprocess_run, args=[setup_command])
-    main_menu.items.append(setup_item)
-
-main_menu.show()
+selection = ctff_functions.print_menu(setup_modules)
+command = "python3 " + setup_modules[selection].setupFile
+subprocess_run(command)
