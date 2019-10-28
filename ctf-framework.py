013333803f140f08af1e2997e9668b5c562470b5
sloftus <sloftus@aegis-strategies.com>
2019-10-28 14:03:09 -0500

    Empty cfg handling

--- ctf-framework.py
+++ ctf-framework.py
@@ -34,8 +34,7 @@ def menu_select(folder):
         if os.path.exists(setup_path):
             subprocess_command = 'python3 ' + setup_path
             subprocess_run(subprocess_command)
-            menu_select(selected_path)
-
+    menu_select(selected_module.modulePath)
     # If we haven't hit exit condition, we print the menu again
     return True
 
