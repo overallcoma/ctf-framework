a625588e30af65e3687ac22e82f082ef7a2345d0
sloftus <sloftus@aegis-strategies.com>
2019-10-28 13:46:05 -0500

    Un-fucked the menu stuff

--- ctf-framework.py
+++ ctf-framework.py
@@ -18,24 +18,25 @@ def clear():
 
 def menu_select(folder):
     config_modules = ctff_functions.config_collect(folder)
-    # clear()
+    clear()
     selection = ctff_functions.print_menu(config_modules)
     print(selection)
-    # This handles the exit selection #
+    # This handles the exit selection - because the return is one number less than what user selects
+    # So if the selection matches the length of the modules array, that's an exit request
     if selection == len(config_modules):
         return False
     selected_module = config_modules[selection]
     if selected_module.moduleType == "folder":
         selected_path = config_modules[selection].modulePath
         menu_select(selected_path)
-        return True
+
     if selected_module.moduleType == "installer":
         selected_path = config_modules[selection].modulePath
         setup_path = os.path.join(selected_path, "setup.py")
         if os.path.exists(setup_path):
             subprocess_command = 'python3 ' + setup_path
             subprocess_run(subprocess_command)
-            return True
+    # If we haven't hit exit condition, we print the menu again
     return True
 
 
