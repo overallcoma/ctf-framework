785ba55f6988144c84681d0d8e74ced004b254f9
OAC <overallcoma@gmail.com>
2019-11-01 13:27:14 -0500

    debugging first challenge

--- ctff_functions/docker_rpcheck.py
+++ ctff_functions/docker_rpcheck.py
@@ -9,13 +9,15 @@ def ctff_rp_check():
     container_list = client.containers.list(all)
     for container in container_list:
         if "nginx-proxy" in container.name:
-            if "jwilder/nginx-proxy" in container.image:
-                if "ctff-deployed" in container.exec_run("env"):
-                    nginx_rp_check = 1
+            if "jwilder/nginx-proxy" in container.image.attrs.get("RepoTags", "none"):
+                nginx_rp_check = 1
+                # if "ctff-deployed" in container.exec_run(cmd="echo $"):
+                #    nginx_rp_check = 1
         if "nginx-proxy-letsencrypt" in container.name:
-            if "jrcs/letsencrypt-nginx-proxy-companion" in container.image:
-                if "ctff-deployed" in container.exec_run("env"):
-                    nginx_rp_letsencrypt_check = 1
+            if "jrcs/letsencrypt-nginx-proxy-companion" in container.image.attrs.get("RepoTags", "none"):
+                nginx_rp_letsencrypt_check = 1
+                # if "ctff-deployed" in container.exec_run("env"):
+                #    nginx_rp_letsencrypt_check = 1
     if nginx_rp_check == 1 and nginx_rp_letsencrypt_check == 1:
         ctff_rp_deployed = 1
     else:
