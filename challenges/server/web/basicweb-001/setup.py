89873dfcba085066873e00dd0cf7f3ffcde5ae6d
OAC <overallcoma@gmail.com>
2019-11-01 15:28:17 -0500

    debugging first challenge

--- challenges/server/web/basicweb-001/setup.py
+++ challenges/server/web/basicweb-001/setup.py
@@ -138,7 +138,7 @@ docker_client = ctff_functions.create_client()
 
 try:
     build_path = os.path.dirname(os.path.realpath(__file__))
-    docker_client.images.build(path=build_path, tag="ctff", buildargs={
+    docker_client.images.build(path=build_path, tag="ctff/basicweb-001", buildargs={
         "var_flagpage": flag_page_name,
         "var_password": password
     })
