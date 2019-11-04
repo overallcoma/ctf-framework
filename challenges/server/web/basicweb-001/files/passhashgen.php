a8fc3a57df1ada7118f620b6b0f789a00c0c50bb
sloftus <sloftus@aegis-strategies.com>
2019-11-04 15:50:59 -0600

    debugging first challenge

--- challenges/server/web/basicweb-001/files/passhashgen.php
+++ challenges/server/web/basicweb-001/files/passhashgen.php
@@ -1,5 +1,5 @@
 <?php
-    $inputfile_content = file_get_contents("/var/www/html/password.txt")
+    $inputfile_content = file_get_contents("/var/www/html/password.txt");
     // $handler = fopen('php://stdin','r') or die($error_message);
     //while($stdin_input = fgets($handler,1024));{
     $outputfile = 'hash.txt';
