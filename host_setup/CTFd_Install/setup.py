5d1b7db288f43ed0528ffd3ce6a7abea8952932f
sloftus <sloftus@aegis-strategies.com>
2019-11-13 13:15:57 -0600

    Testing CTFd Deployment

--- host_setup/CTFd_Install/setup.py
+++ host_setup/CTFd_Install/setup.py
@@ -90,7 +90,7 @@ yaml_data['services']['ctfd']['volumes'] = volume_replace
 
 # Put the containers on the default network
 # docker_client.networks.create("ctfd_internal", internal=True, attachable=True)
-yaml_data['networks'] = {"default": {'external': {'name' : 'bridge'}} , 'internal': {'internal': 'true'}}
+yaml_data['networks'] = {"default": {'external': {'name' : 'bridge'}}, 'internal': {'internal': 'true'}}
 # yaml_data['networks']['default'] = {'external': {'name': 'bridge'}, 'internal': {'internal': 'true'}}
 # yaml_data['networks']['internal'] = {'external': {'name': 'ctfd_internal'}}
 # Change the YAML version - Can't have a renamed internal network without this
