d886c135e9d92b3849b45359d034d0c20d156402
sloftus <sloftus@aegis-strategies.com>
2020-01-27 16:11:49 -0600

    New stuff with a new technique

--- challenges/web/theraven-001/build/target-01/docker-entrypoint.sh
+++ challenges/web/theraven-001/build/target-01/docker-entrypoint.sh
@@ -3,5 +3,8 @@
 /usr/sbin/iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 /usr/sbin/iptables -A INPUT -p tcp --dport 22 -j REJECT
 /usr/sbin/knockd -d
+echo "AllowUsers ${GUEST_USERNAME}"
+useradd -m -s /bin/bash ${GUEST_USERNAME}
+echo "${GUEST_USERNAME}:${GUEST_PASSWORD}" | chpasswd
 /usr/sbin/sshd
 /usr/bin/tail -f /var/log/knockd.log
